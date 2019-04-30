e89d6304e4720eb680de0c6563d8a01c819aba08
timi <tangchengli@icourt.cc>
2019-04-30 22:35:41 +0800

    更新注释

--- src/services/word.service.ts
+++ src/services/word.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Repository, FindManyOptions } from 'typeorm';
 import { Word } from './../entities/word.entity';
 
 @Injectable()
@@ -10,18 +10,28 @@ export class WordService {
     private readonly wordRepository: Repository<Word>,
   ) { }
 
-  // 保存新单词
+  /**
+   * 保存新单词
+   * @param data 新单词
+   */
   async save(data: any): Promise<any> {
     return await this.wordRepository.save(data);
   }
 
-  // 更新单词
+  /**
+   * 更新单词
+   * @param data 单词
+   */
   async update(data: any): Promise<any> {
     return await this.wordRepository.update(data.id, data);
   }
 
-  // 查找所有单词
-  async findAll(): Promise<Word[]> {
-    return await this.wordRepository.find();
+  /**
+   * 查找所有单词
+   * @param params 查询参数
+   */
+  async findAll(params: any): Promise<[Word[], number]> {
+    const param: FindManyOptions<Word> = {skip: params.pageSize * params.currentPage, take: params.pageSize};
+    return await this.wordRepository.findAndCount(param);
   }
 }
